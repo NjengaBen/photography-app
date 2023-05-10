19d880d987163532093e5c3a0adb4555e19d5af4
NjengaBen <bennjenga2@gmail.com>
2023-05-10 23:58:24 +0300

    initial changes

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
+      {/* overlay */}
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]" />
+      <div className="text-white p-5 z-[2] ml-[-10rem] mt-[-10rem]">
+        <h2 className="text-5xl font-bold">Heading</h2>
+        <p className="text-xl py-5">Message</p>
+        <button className="py-2 px-8 border">Book</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
