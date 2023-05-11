08e561d0bd0ddc4187fa57edcfc4f88f31e1f96e
NjengaBen <bennjenga2@gmail.com>
2023-05-11 03:05:47 +0300

    navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div>
-      <Hero />
+      <Hero
+        heading="Captur Photography"
+        message="I capture moments in nature and keep them alive"
+      />
+      {/* <Navbar /> */}
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const Hero = () => {
+const Hero = ({ heading, message }) => {
   return (
     <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
       {/* overlay */}
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]" />
-      <div className="text-white p-5 z-[2] ml-[-10rem] mt-[-10rem]">
-        <h2 className="text-5xl font-bold">Heading</h2>
-        <p className="text-xl py-5">Message</p>
+      <div className="text-white p-5 z-[2] mt-[-10rem]">
+        <h2 className="text-5xl font-bold">{heading}</h2>
+        <p className="text-xl py-5">{message}</p>
         <button className="py-2 px-8 border">Book</button>
       </div>
     </div>
